5b1c84c3f04efe1ac991145b368830ffb8bbef8e
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-08-25 00:55:06 +0800

    Initialize framework with Tailwind CSS setup, including package.json, PostCSS configuration, and initial CSS files. Add Tailwind configuration for content paths.

---
+++ framework/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.{html,twig}",
+    "./public/**/*.{html,js}",
+    "./app/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
